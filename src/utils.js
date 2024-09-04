a4de7dc3528259e7b9240e84dd1a8d1f62f1c919
cristianpastorini <cristian_pastorini@hotmail.com.ar>
2024-09-03 23:42:13 -0300

    mi segundo commit

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+export default __dirname
